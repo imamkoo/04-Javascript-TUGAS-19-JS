ba70d0be6acb51092fab81b762d52aac717f920f
Imamkoo <imammm.kooo@gmail.com>
2021-12-18 16:44:38 +0700

    Tugas 19 Javascript

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+let isi = "Semangat...";
+console.log(isi.repeat(5));
+console.log("----------------------------------------------------");
+console.log('*****'.repeat(1));
+console.log('*****'.repeat(4));
+console.log('*****'.repeat(8));
+console.log('*****'.repeat(12));
+console.log('*****'.repeat(16));
+console.log('*****'.repeat(12));
+console.log('*****'.repeat(8));
+console.log('*****'.repeat(4));
+console.log('*****'.repeat(1));
